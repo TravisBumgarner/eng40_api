be214aded9f67563adcab7a030bae9aad845fde0
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-03-04 00:14:33 -0500

    Added pushover, author row to DB

--- api/utils.py
+++ api/utils.py
@@ -1,13 +1,14 @@
-from settings.pushover_config import user, token
-import http.client, urllib
+from eng40.keys import PUSHOVER_APP_TOKEN, PUSHOVER_USER_KEY
+import httplib
+import urllib
 
 
 def send(message):
-    conn = http.client.HTTPSConnection("api.pushover.net:443")
+    conn = httplib.HTTPSConnection("api.pushover.net:443")
     conn.request("POST", "/1/messages.json",
-      urllib.parse.urlencode({
-        "token": token,
-        "user": user,
-        "message": message,
-      }), { "Content-type": "application/x-www-form-urlencoded" })
+        urllib.parse.urlencode({
+            "token": PUSHOVER_APP_TOKEN,
+            "user": PUSHOVER_USER_KEY,
+            "message": message,
+        }), { "Content-type": "application/x-www-form-urlencoded" })
     conn.getresponse()
