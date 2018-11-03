4e14d2bd09504611b5fbc6e4b9ee03952f2c2f03
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-11-03 16:46:35 +0000

    (api) fixed send message

--- api/utils.py
+++ api/utils.py
@@ -1,15 +1,14 @@
-from eng40.keys import PUSHOVER_APP_TOKEN, PUSHOVER_USER_KEY
-import httplib
-import urllib
 
+from keys import PUSHOVER_APP_TOKEN, PUSHOVER_USER_KEY
 
+import httplib, urllib
 def send(message):
     conn = httplib.HTTPSConnection("api.pushover.net:443")
     conn.request("POST", "/1/messages.json",
-        urllib.urlencode({
-            "token": PUSHOVER_APP_TOKEN,
-            "user": PUSHOVER_USER_KEY,
-            "message": message,
-        }), { "Content-type": "application/x-www-form-urlencoded" })
-    conn.getresponse()
-
+      urllib.urlencode({
+          "token": PUSHOVER_APP_TOKEN,
+          "user": PUSHOVER_USER_KEY,        
+          "message": message,
+      }), { "Content-type": "application/x-www-form-urlencoded" })
+    r = conn.getresponse()
+    return r
